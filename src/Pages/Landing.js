e1f1753ff034ba27715e8fa3913e1c81521d2d98
shahzaib saleem <shahzaibsaleem665@gmail.com>
2024-04-05 11:03:53 +1100

    getting there with styling

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
 import './Landing.css'
+import Divider from '@mui/material/Divider';
+import google from '../assets/google.svg'
+import apple from '../assets/apple.svg'
+import { Avatar, Button} from '@mui/material';
 
 function Landing() {
   return (
@@ -12,19 +16,23 @@ function Landing() {
 
         {/* Below code is for the right side of the page */}
         <div className="landing__right">
-            <h1>Happening Now</h1>
-
             {/* All the buttons and sign in options in the below divs */}
 
             <div className="landing__rightOptions">
+                <div className="landing__rightOptionsTop">
+                <h1>Happening now</h1>
                 <h2>Join today.</h2>
+                </div>
+
+
                 <div className="landing__rightOptionsMiddle">
-                <button>Google</button>
-                <button>Apple</button>
-             
+                <button > <Avatar className='avatar' src={google} />Sign up with Google </button>
+                <button> <Avatar className='avatar' src={apple} />Sign up with Apple</button>
+             <Divider orientation='horizontal' variant='full-width'>or</Divider>
                 <button>Create account</button>
                 <p>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</p>
                 </div>
+
 
                 <div className="landing__rightOptionsBottom">
                 <h3>Already have an account?</h3>
